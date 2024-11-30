1511392e847ec28c964823704ab284524328c6ca
lyon86 <torres.lionel@gmail.com>
2024-11-30 18:37:20 +0100

    Update language-switcher.js

--- language-switcher.js
+++ language-switcher.js
@@ -1,17 +1,13 @@
-// Function to change language
 function changeLanguage() {
   const selectedLanguage = document.getElementById('language-select').value;
 
-  // Store the selected language in local storage
   localStorage.setItem('selectedLanguage', selectedLanguage);
 
-  // Update text for each element with data attributes for languages
   document.querySelectorAll('[data-en][data-es]').forEach((el) => {
     el.innerText = el.getAttribute(`data-${selectedLanguage}`);
   });
 }
 
-// Function to load the stored language on page load
 function loadLanguage() {
   const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
   document.getElementById('language-select').value = savedLanguage;
@@ -21,5 +17,4 @@ function loadLanguage() {
   });
 }
 
-// Load the language when the page is loaded
 document.addEventListener('DOMContentLoaded', loadLanguage);
