eca9dd4f971a094816d5302468688fb9c3881def
lyon86 <torres.lionel@gmail.com>
2024-11-30 18:29:40 +0100

    Update language-switcher.js
    persistent language

--- language-switcher.js
+++ language-switcher.js
@@ -1,8 +1,25 @@
+// Function to change language
 function changeLanguage() {
   const selectedLanguage = document.getElementById('language-select').value;
+
+  // Store the selected language in local storage
+  localStorage.setItem('selectedLanguage', selectedLanguage);
 
   // Update text for each element with data attributes for languages
   document.querySelectorAll('[data-en][data-es]').forEach((el) => {
     el.innerText = el.getAttribute(`data-${selectedLanguage}`);
   });
 }
+
+// Function to load the stored language on page load
+function loadLanguage() {
+  const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+  document.getElementById('language-select').value = savedLanguage;
+
+  document.querySelectorAll('[data-en][data-es]').forEach((el) => {
+    el.innerText = el.getAttribute(`data-${savedLanguage}`);
+  });
+}
+
+// Load the language when the page is loaded
+document.addEventListener('DOMContentLoaded', loadLanguage);
