840d0315d20d1e0211837bc584d111bb0165ad67
lyon86 <torres.lionel@gmail.com>
2024-11-30 18:46:18 +0100

    Update language-switcher.js

--- language-switcher.js
+++ language-switcher.js
@@ -1,13 +1,17 @@
+// Function to change language
 function changeLanguage() {
   const selectedLanguage = document.getElementById('language-select').value;
 
+  // Store the selected language in local storage
   localStorage.setItem('selectedLanguage', selectedLanguage);
 
+  // Update text for each element with data attributes for languages
   document.querySelectorAll('[data-en][data-es]').forEach((el) => {
     el.innerText = el.getAttribute(`data-${selectedLanguage}`);
   });
 }
 
+// Function to load the stored language on page load
 function loadLanguage() {
   const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
   document.getElementById('language-select').value = savedLanguage;
@@ -17,4 +21,5 @@ function loadLanguage() {
   });
 }
 
+// Load the language when the page is loaded
 document.addEventListener('DOMContentLoaded', loadLanguage);
